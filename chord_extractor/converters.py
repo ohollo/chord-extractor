5f055d26e310497aaa0918416ed207e7f1ee1e5e
Kinyugo <kinyugokyde@gmail.com>
2025-02-12 23:12:05 +0300

    Add verbose option to disable timidity output in midi_to_wav

--- chord_extractor/converters.py
+++ chord_extractor/converters.py
@@ -17,14 +17,14 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+import logging
 import os
 import subprocess
-import logging
 
 _log = logging.getLogger(__name__)
 
 
-def midi_to_wav(midi_path: str, wav_to_dir: str):
+def midi_to_wav(midi_path: str, wav_to_dir: str, verbose: bool = True):
     """
     Convert midi at given path to wav file and save in specified output directory. This is done using
     Timidity (http://timidity.sourceforge.net/). If an invalid midi file is passed, note that no exception is raised
@@ -32,17 +32,32 @@ def midi_to_wav(midi_path: str, wav_to_dir: str):
 
     :param midi_path: Path to input midi file
     :param wav_to_dir: Path to the output directory
+    :param verbose: Whether to print timidity output
     :return: Path to the new wav file
     """
     base = os.path.basename(midi_path)
     file_name = os.path.splitext(base)[0]
     wav_file = os.path.join(wav_to_dir, file_name)
     if not os.path.isfile(wav_file):
-        _log.info('Running timidity on {} to create {}'.format(midi_path, wav_file))
-        result = subprocess.run(['timidity', midi_path, '-Ow', '-o', wav_file], stdout=subprocess.PIPE, text=True, errors="replace")
-        if "Not a MIDI file!" in result.stdout:
-            _log.error('Invalid midi file at {}'.format(midi_path))
+        _log.info("Running timidity on {} to create {}".format(midi_path, wav_file))
+        if verbose:
+            subprocess.run(
+                ["timidity", midi_path, "-Ow", "-o", wav_file],
+                stdout=subprocess.PIPE,
+                text=True,
+                errors="replace",
+            )
+        else:
+            subprocess.run(
+                ["timidity", midi_path, "-idqqq", "-Ow", "-o", wav_file],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                text=True,
+                errors="replace",
+            )
+        if not os.path.isfile(wav_file):
+            _log.error("Invalid midi file at {}".format(midi_path))
             return None
     else:
-        _log.info('Returning already existing temporary wav file {}'.format(wav_file))
+        _log.info("Returning already existing temporary wav file {}".format(wav_file))
     return wav_file
